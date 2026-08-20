fbc037c100176a0f124c94d3917e8c933aa875af
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2026-08-20 18:52:23 -0300

    fix: ruff

--- foodmenuapp/wsgi.py
+++ foodmenuapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodmenuapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodmenuapp.settings")
 
 application = get_wsgi_application()
