2689f988deacac700a0ac4b6a6e4e49576b357b0
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2026-08-22 11:05:33 -0300

    feat: adding images for foods

--- base/models.py
+++ base/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    def __str__(self):
+        return self.item_name
+
     item_name = models.CharField(max_length=200)
     item_description = models.CharField()
     item_price = models.IntegerField()
-
-    def __str__(self):
-        return self.item_name
+    item_image = models.CharField(
+        max_length=500,
+        default="https://www.runawayapricot.com/wp-content/uploads/2014/09/placeholder.jpg",
+    )
