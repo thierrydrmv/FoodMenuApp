cb0af5cc49d1d697f9ec7b9562c349cc2e42a90e
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2026-09-02 23:30:57 -0300

    feat: add pagination to the api

--- foodmenuapp/settings.py
+++ foodmenuapp/settings.py
@@ -173,6 +173,8 @@ LOGGING = {
 }
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
